d9841541eb8fbe2a75be5c65d677fcb9d5a2b525
Maria Borisova <mborisovaitd@yandex.ru>
2024-01-14 15:35:04 +0300

    Added API Orders endpoint, renamed user delete/patch endpoint

--- src/main/java/ru/constants/Api.java
+++ src/main/java/ru/constants/Api.java
@@ -2,7 +2,10 @@ package ru.constants;
 
 public class Api {
     public static final String BASE_URI = "https://stellarburgers.nomoreparties.site";
+
     public static final String USER_REGISTER_API_ENDPOINT = "/api/auth/register";
     public static final String USER_LOGIN_API_ENDPOINT = "/api/auth/login";
-    public static final String DELETE_USER_API_ENDPOINT = "/api/auth/user";
+    public static final String USER_DELETE_OR_PATCH_API_ENDPOINT = "/api/auth/user";
+
+    public static final String ORDERS_API_ENDPOINT = "/api/orders";
 }
